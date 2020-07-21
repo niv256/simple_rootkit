f550ad50673b4696a1cc9146d4b55b11ffe17de6
niv256 <niv25600@gmail.com>
2020-07-21 12:12:32 +0300

    Added licensing and module documentation.

--- simple_rootkit.c
+++ simple_rootkit.c
@@ -2,6 +2,15 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 
+#define DRIVER_NAME "simple_rootkit"
+#define DRIVER_AUTHOR "Niv Meiran and Tal Gorbunov"
+#define DRIVER_DESC "A simple, multi-functional, linux rootkit"
+#define DRIVER_LICENSE "GPL"
+
+MODULE_LICENSE(DRIVER_LICENSE);
+MODULE_AUTHOR(DRIVER_AUTHOR);
+MODULE_DESCRIPTION(DRIVER_DESC);
+
 static int __init rootkit_init(void) {
 	printk(KERN_INFO "[+] Module loaded, inside %s.\n", __FUNCTION__);
 	return 0;
