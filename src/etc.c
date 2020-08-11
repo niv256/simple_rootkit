8c53588c66c55f66a7fc335732ba43a486fde328
niv256 <niv25600@gmail.com>
2020-08-11 18:25:11 +0300

    Fix indentation in etc.c

--- src/etc.c
+++ src/etc.c
@@ -4,29 +4,29 @@
 // set page holding _addr to read write
 int set_addr_rw(unsigned long _addr) {
 
-        unsigned int level;
-        pte_t *pte;
+	unsigned int level;
+	pte_t *pte;
 
-        pte = lookup_address(_addr, &level);
+	pte = lookup_address(_addr, &level);
 
-		// set read write in page attributes
-        if (pte->pte &~ _PAGE_RW) {
-                pte->pte |= _PAGE_RW;
-        }
+	// set read write in page attributes
+	if (pte->pte &~ _PAGE_RW) {
+		pte->pte |= _PAGE_RW;
+	}
 
-        return 0;
+	return 0;
 }
 
 // set page holding _addr to read only
 int set_addr_ro(unsigned long _addr) {
 
-        unsigned int level;
-        pte_t *pte;
+	unsigned int level;
+	pte_t *pte;
 
-        pte = lookup_address(_addr, &level);
+	pte = lookup_address(_addr, &level);
 
-		// unset read write in page attributes
-        pte->pte = pte->pte &~_PAGE_RW;
+	// unset read write in page attributes
+	pte->pte = pte->pte &~_PAGE_RW;
 
-        return 0;
+	return 0;
 }
